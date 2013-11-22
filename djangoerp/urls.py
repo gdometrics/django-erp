81e45f066d6b09cdb0c1201adc9152b573b6b769
Emanuele Bertoldi <emanuele.bertoldi@gmail.com>
2013-11-22 19:22:05 +0800

    Fixed "admindocs" URL handling but removed it from default installed apps.

--- djangoerp/urls.py
+++ djangoerp/urls.py
@@ -22,11 +22,11 @@ from django.conf import settings
 urlpatterns = patterns('',)
 
 if 'django.contrib.admin' in settings.INSTALLED_APPS:
+  if 'django.contrib.admindocs' in settings.INSTALLED_APPS:
+    urlpatterns += patterns('', (r'^admin/doc/', include('django.contrib.admindocs.urls')))
   from django.contrib import admin
   admin.autodiscover()
   urlpatterns += patterns('', (r'^admin/', include(admin.site.urls)))
-  if 'django.contrib.admindocs' in settings.INSTALLED_APPS:
-    urlpatterns += patterns('', (r'^admin/doc/', include('django.contrib.admindocs.urls')))
 
 if 'django.contrib.staticfiles' in settings.INSTALLED_APPS:
   from django.contrib.staticfiles.urls import staticfiles_urlpatterns
