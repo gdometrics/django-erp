e482b57d2ca404daf3cf670da0df856f526a518a
Emanuele Bertoldi <emanuele.bertoldi@gmail.com>
2013-12-06 22:46:25 +0800

    Added test for "core.forms.auth.UserForm".

--- djangoerp/core/tests/test_forms.py
+++ djangoerp/core/tests/test_forms.py
@@ -16,11 +16,13 @@ __copyright__ = 'Copyright (c) 2013 Emanuele Bertoldi'
 __version__ = '0.0.2'
 
 from django.test import TestCase
-from django.forms import Form, ModelForm
+from django.forms import Form, ModelForm, ValidationError
+from django.utils.encoding import force_text
 from django.contrib.admin.helpers import AdminForm
 from django.contrib.auth import get_user_model
 
 from ..forms import *
+from ..forms.auth import *
 
 class EnrichFormTestCase(TestCase):
     def test_enrich_form_class(self):
@@ -79,3 +81,56 @@ class EnrichFormTestCase(TestCase):
         self.assertEqual(f.required_css_class, "required")
         self.assertTrue(hasattr(f, "error_css_class"))
         self.assertEqual(f.error_css_class, "errors")
+        
+class UserFormTestCase(TestCase):
+    def setUp(self):
+        self.user = get_user_model().objects.create(username="u1", email="u@u.it")
+        self.user_data = {'username': 'u1', 'email': 'u@u.it', 'timezone': 'GMT', 'language': 'en'}
+        
+    def test_raise_exception_when_no_password1_on_creation(self):
+        """Tests raising an error when no password is provided on user creation.
+        """
+        f = UserForm()
+        
+        self.assertTrue(f.fields['password1'].required)
+        self.assertTrue(f.fields['password2'].required)
+        self.assertFalse(f.is_valid())
+        
+    def test_no_password_required_on_editing(self):
+        """Tests no password is required on user editing.
+        """
+        f = UserForm(self.user_data, instance=self.user)
+        
+        self.assertFalse(f.fields['password1'].required)
+        self.assertFalse(f.fields['password2'].required)
+        self.assertTrue(f.is_valid())
+        
+    def test_clean_password1(self):
+        """Tests cleaning password's value.
+        """
+        f = UserForm({"password1": "password"}, instance=self.user)
+        
+        f.is_valid()
+              
+        self.assertEqual(f.cleaned_data['password1'], "password")
+        
+    def test_clean_password2(self):
+        """Tests cleaning password's value.
+        """
+        test_data = self.user_data
+        test_data.update({"password1": "password", "password2": "password"})
+        
+        f = UserForm(test_data, instance=self.user)
+        
+        self.assertTrue(f.is_valid())
+        
+    def test_error_when_passwords_are_not_equal(self):
+        """Tests an error must be raised if password values are not equal.
+        """
+        test_data = self.user_data
+        test_data.update({"password1": "password", "password2": "password2"})
+        
+        f = UserForm(test_data, instance=self.user)
+        
+        self.assertFalse(f.is_valid())
+        
